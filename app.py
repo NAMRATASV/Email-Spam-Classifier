47ee74bd32e7a8fa0a9817e0af073813f873521c
NAMRATASV <64409091+NAMRATASV@users.noreply.github.com>
2025-01-08 17:12:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import sklearn
-nltk.download('punkt_tab')
+nltk.download('punkt')
+nltk.download('stopwords')
 
 # vectorizer
 vectorizer_path = 'Vectorizer.pkl'
